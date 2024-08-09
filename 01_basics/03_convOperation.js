8b37b95ceb743c2a3ddb0032909a0a0b80c9896f
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-08-09 05:55:00 +0000

    conversion confusion fixed

---
+++ 01_basics/03_convOperation.js
@@ -0,0 +1,77 @@
+let score = "Yawar Abbas";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber= Number(score);
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "Yawar Abbas";
+
+let BooleanIsLoggedIn= Boolean(isLoggedIn);
+// console.log(BooleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "yawar" => true
+
+
+let someNum=33;
+let stringNum=String(someNum);
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+
+// ************ operations *************//
+let value = 3;
+let negValue= -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="hello";
+let str2=" yawar abbas";
+let str3= str1+str2;
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(3+5*3%3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1,num2,num3;
+// num1=num2=num3=2+2;
+
+// prefix/postfix operators
+let gameCounter=100;
+// gameCounter++;
+++gameCounter;
+
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
