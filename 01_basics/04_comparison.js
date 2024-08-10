24c1a484ae2838bfea05683ab56c4badfe95794d
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-08-10 11:35:46 +0000

    comparion done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2>=1);
+// console.log(2<=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+// ==== 
+// checks same data types + value
+console.log("2"===2);
