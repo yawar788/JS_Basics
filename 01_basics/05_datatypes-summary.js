7a94e838ebde4f6ac1b670ef1e805dfb1b625b03
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-08-11 12:13:31 +0000

    datatypes summary

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,53 @@
+const score=100;
+const scoreValue=100.3;
+
+const isLoggedIn = false;
+const outsideTemp= null;  
+let userEmail;
+const id= Symbol("123");
+const anotherId= Symbol("123");
+
+// console.log(id===anotherId);
+// const bigNumber=2131231243124421421n;
+
+const names=["Ali","Naseem"];
+let Obj={
+    name:"yawar",
+    age:20,
+    city:"sukkur"
+}
+
+let myFunction=function(){
+    console.log("Hello function");
+    
+}
+// myFunction();
+
+console.log(typeof names);
+
+
+
+
+
+
+
+// primitive 
+// non-primitive/reference type
+
+// 7 types: string, number, boolean, null, symbol, bigint
+// Reference (non primitive)
+
+// arrays, objects, functions
+// js is dynamically typed 
+
+// Question:
+// Is JavaScript a statically or a dynamically typed language?
+
+// Quick answer:
+// JavaScript is a dynamically typed language, but TypeScript is a statically typed language.
+
+// Longer answer:
+// In dynamically typed languages all type checks are performed in a runtime, only when your 
+// program is executing. So this means you can just assign anything you want to the variable and 
+// it will work.
+
