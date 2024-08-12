b495f747c2094bbbf40e20c355ada82c70d41885
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-08-12 11:13:02 +0000

    trim mdn added

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -22,6 +22,10 @@ const toTrimString="    yawar    ";
 console.log(toTrimString);
 console.log(toTrimString.trim());
 
+// The trim() method of String values removes whitespace from both ends of this string 
+// and returns a new string, without modifying the original string.
+// To return a new string with whitespace trimmed from just one end, use trimStart() or trimEnd().
+
 const url ="https://yawarabbas.com/yawar%20meerani";
 console.log(url.replace("%20","-"));
 console.log(url.includes("yawarabbas"));
