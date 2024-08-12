67f8c76e745c0c20f58001222dc3fdd6b5e6e178
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-08-12 11:23:05 +0000

    replace added

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -32,6 +32,25 @@ console.log(url.includes("yawarabbas"));
 
 console.log(gameName.split("-"));
 
+// The split() method of String values takes a pattern and divides this string into an
+// ordered list of substrings by searching for the pattern, puts these substrings into
+// an array, and returns the array.
+
+// const str = 'The quick brown fox jumps over the lazy dog.';
+
+// const words = str.split(' '); // an array
+// console.log(words[3]);
+// // Expected output: "fox"
+
+// const chars = str.split('');
+// console.log(chars[8]);
+// // // Expected output: "k"
+
+// const strCopy = str.split();
+// console.log(strCopy);
+// // Expected output: Array ["The quick brown fox jumps over the lazy dog."]
+
+
 
 
 
