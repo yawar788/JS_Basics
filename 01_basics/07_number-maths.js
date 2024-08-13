ec3e33732753a9df231b9b781cab5923e370daea
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-08-13 06:36:41 +0000

    maths and number done

---
+++ 01_basics/07_number-maths.js
@@ -0,0 +1,23 @@
+const score = 400;
+// console.log(score);
+
+const balance= new Number(100);
+// console.log(balance);   
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // decimal places
+
+const otherNum=23.45353;
+// console.log(otherNum.toPrecision(4));
+
+const hundreds = 100000;
+// console.log(hundreds.toLocaleString()); // adds commas us standards 
+
+//******************************************* Maths ************************************
+
+// maths is the library in js 
+
+
+
+
+
+ 
