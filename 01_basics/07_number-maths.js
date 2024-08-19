f3a3ef1e3f5f8e5a29dee2d7bef4097d9456f8ce
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-08-19 19:23:10 +0000

    number and maths done

--- 01_basics/07_number-maths.js
+++ 01_basics/07_number-maths.js
@@ -1,20 +1,46 @@
-const score = 400;
+const score = 400; // random number
 // console.log(score);
 
-const balance= new Number(100);
+const balance= new Number(100.222); // fix nuber type
 // console.log(balance);   
 // console.log(balance.toString().length);
-// console.log(balance.toFixed(2)); // decimal places
-
+// console.log(balance.toFixed(2)); // to fix decimal places
 const otherNum=23.45353;
-// console.log(otherNum.toPrecision(4));
+// console.log(otherNum.toPrecision(2));
 
 const hundreds = 100000;
 // console.log(hundreds.toLocaleString()); // adds commas us standards 
+// console.log(hundreds.toLocaleString("en-IN"));// indian standard
+
 
 //******************************************* Maths ************************************
 
 // maths is the library in js 
+// maths is the by default libray in js 
+
+// console.log(Math);
+// console.log(Math.abs(-1));
+// console.log(Math.round(2.1));
+// console.log(Math.ceil(1.2)); // takes top value 
+// console.log(Math.floor(1.2)); // takes lowest value 
+// console.log(Math.min(2,2,5,6)); 
+// console.log(Math.max(2,2,5,6));  
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) +1);
+
+const min =10;
+const max =20;
+
+console.log(Math.floor(Math.random()*(max-min +1))+min);
+
+
+
+
+
+
+
+
+
 
 
 
