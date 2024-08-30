f54c32943e1a9b2ed5bd397015ce42ecacd28ccf
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-08-30 19:46:04 +0000

    array half done

---
+++ 02_basics/01_array.js
@@ -0,0 +1,18 @@
+// array
+ let myArr =[0,1,2,3,4,5];
+//  console.log(myArr[3]);
+ 
+ let myHeroes= ["Tony Shark","Iron Man"];
+ let myArr2= new Array(1,2,3,4);
+
+ // array methods
+
+// myArr.push(6);
+// myArr.push(7);
+// myArr.pop(); // removes last value
+// myArr.unshift(9) ;
+// myArr.shift();
+console.log(myArr.includes(9));
+// console.log(myArr);
+
+ 
