cb8c9895b91d826f981a65a2cc9b52889071beba
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-08-31 20:08:30 +0000

    array changes

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,9 +1,9 @@
 const marvel_heros=["Marvel", "Thor","Iron Man"];
 const dc_heros=["Batman", "Superman","flash"];
 
-// marvel_heros.push(dc_heros);
+marvel_heros.push(dc_heros);
 // console.log(marvel_heros);
-// console.log(marvel_heros[3][1]);
+// console.log(marvel_heros[3][1]); // Superman
 
 const allHeros=marvel_heros.concat(dc_heros);
 // console.log(allHeros);
@@ -11,14 +11,17 @@ const allHeros=marvel_heros.concat(dc_heros);
 // spread operator values
 
 const allNewHeros=[...marvel_heros,...dc_heros];
+// console.log(allHeros);
 // console.log(typeof allNewHeros);
 
-const array1=[1,2,3,[4,5,6],7,[7,6,[4,5]]];
+const array1=[1,2,3,[4,5,6],7,[7,6,[4,5]]]; 
 const array2=array1.flat(Infinity);
 // console.log(array2);
 
-// console.log(Array.isArray("Yawar Abbas"));
+// console.log(Array.isArray("Yawar Abbas")); // false 
 // console.log(Array.from("Yawar Abbas"));
+// console.log(Array.of("Yawar Abbas"));
+
 // console.log(Array.from({name:"Yawar Abbas"})); // interesting 
 
 let score1=100;
