f6dce1cd025f80567081d7e6c370915f168219df
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-09-02 21:56:35 +0000

    function explains

---
+++ 03_basics/01_function.js
@@ -0,0 +1,39 @@
+// function - some lines of code close in pacakage 
+// takes copies and use it
+
+// defination
+function sayMyname(){
+
+console.log("a");
+console.log("b");
+console.log("b");
+console.log("a");
+console.log("s");
+}
+
+// sayMyname -->  reference
+// sayMyname() // --> execution
+
+
+// add two nums 
+
+// function addTwoNum(num1,num2){
+// console.log(num1+num2);
+
+// }
+
+function addTwoNum(num1,num2){
+//    let result =num1+num2;
+//    return result;
+//    return num1+num2;
+// if return then value can be save in varizle 
+    // console.log("yawar"); // not execute
+    
+    }
+
+// addTwoNum(1,2);
+
+const result=addTwoNum(1,4);
+console.log("result:",result); // undefined   
+
+
