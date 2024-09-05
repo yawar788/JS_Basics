b51c1e76c0e133434cd861728ff9f605340f37bd
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-09-05 19:28:34 +0000

    more about function plus if/else

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -2,14 +2,14 @@
 // takes copies and use it
 
 // defination
-function sayMyname(){
+// function sayMyname(){
 
-console.log("a");
-console.log("b");
-console.log("b");
-console.log("a");
-console.log("s");
-}
+// console.log("a");
+// console.log("b");
+// console.log("b");
+// console.log("a");
+// console.log("s");
+// }
 
 // sayMyname -->  reference
 // sayMyname() // --> execution
@@ -22,18 +22,35 @@ console.log("s");
 
 // }
 
-function addTwoNum(num1,num2){
+// function addTwoNum(num1,num2){
 //    let result =num1+num2;
 //    return result;
 //    return num1+num2;
 // if return then value can be save in varizle 
     // console.log("yawar"); // not execute
     
-    }
+    // }
 
 // addTwoNum(1,2);
 
-const result=addTwoNum(1,4);
-console.log("result:",result); // undefined   
+// const result=addTwoNum(1,4);
+// console.log("result:",result); // undefined   
 
+function LoginUserMessage(username="sam"){
+    // if(username===undefined){
+    //     console.log("Please enter a username");
+    //     return // no code execute after it 
+    // }
+
+    if(!username){
+        console.log("Please enter a username");
+        return // no code execute after it 
+    }
+    return `${username} just logged in `
+} 
+
+// console.log(LoginUserMessage("Yawar"));
+console.log(LoginUserMessage());
+
+ 
 
