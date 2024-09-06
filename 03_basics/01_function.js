3720c49a9b8ac5e23ff0a7dc0ec35de931d82437
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-09-06 19:11:23 +0000

    functions with objects and arrays done

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -1,7 +1,10 @@
+// function-part 1
 // function - some lines of code close in pacakage 
 // takes copies and use it
 
 // defination
+
+// 1
 // function sayMyname(){
 
 // console.log("a");
@@ -17,11 +20,12 @@
 
 // add two nums 
 
+// 2
 // function addTwoNum(num1,num2){
 // console.log(num1+num2);
-
 // }
 
+// 3
 // function addTwoNum(num1,num2){
 //    let result =num1+num2;
 //    return result;
@@ -36,21 +40,63 @@
 // const result=addTwoNum(1,4);
 // console.log("result:",result); // undefined   
 
-function LoginUserMessage(username="sam"){
+// 4
+// function LoginUserMessage(username="sam"){
     // if(username===undefined){
     //     console.log("Please enter a username");
     //     return // no code execute after it 
     // }
 
-    if(!username){
-        console.log("Please enter a username");
-        return // no code execute after it 
-    }
-    return `${username} just logged in `
-} 
+    // 5
+    // if(!username){
+    //     console.log("Please enter a username");
+    //     return // no code execute after it 
+    // }
+    // return `${username} just logged in `
+// } 
 
 // console.log(LoginUserMessage("Yawar"));
-console.log(LoginUserMessage());
+// console.log(LoginUserMessage());
 
- 
+// function-part 2
+// shopping cart --> multiple items added in cart concept 
+// rest operator 
+
+// rest / spread operator --> ...
+// diff --> depend on use case 
+// 6 
+// function calculateCartPrice(val1,val2,val3,...num1){
+//     return num1;
+// }
+
+// console.log(calculateCartPrice(22,22,333,2));
+// object 
+const user={
+    name: "Yawar Abbas",
+    prices:200
+}
+
+function handleObject(anyObject){
+    console.log(`usernmae is ${anyObject.name} and price is ${anyObject.price}`);
+}
+
+// handleObject(user);
+handleObject({
+    name:"Alice",
+    price:344
+}); // direct pass object 
+
+
+// array 
+const myNewArr=[1,2,3,4,5];
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArr));
+console.log(returnSecondValue([4,3,4,5])); // direct array pass 
+
+
+
+
 
