fe2e9e77467c6fe06052fe894c1ac27f0533015c
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-09-14 08:04:19 +0000

    strings part-1 revision done

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -9,38 +9,38 @@ const gameName="PUBG-YAWAR-COM";
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
 // console.log(gameName.toLocaleLowerCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf("P"));
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("P"));
 
 const newString=gameName.substring(0,2);
-console.log(newString);
+// console.log(newString);
 
-const anotherString=gameName.slice(-3,2);
-console.log(anotherString);
+const anotherString=gameName.slice(3,6);
+// console.log(anotherString);
 
 const toTrimString="    yawar    ";
-console.log(toTrimString);
-console.log(toTrimString.trim());
+// console.log(toTrimString);
+// console.log(toTrimString.trim());
 
 // The trim() method of String values removes whitespace from both ends of this string 
 // and returns a new string, without modifying the original string.
 // To return a new string with whitespace trimmed from just one end, use trimStart() or trimEnd().
 
 const url ="https://yawarabbas.com/yawar%20meerani";
-console.log(url.replace("%20","-"));
-console.log(url.includes("yawarabbas"));
+// console.log(url.replace("%20","-"));
+// console.log(url.includes("yawarabbas"));
 
-console.log(gameName.split("-"));
+// console.log(gameName.split("-"));
 
 // The split() method of String values takes a pattern and divides this string into an
 // ordered list of substrings by searching for the pattern, puts these substrings into
 // an array, and returns the array.
 
-// const str = 'The quick brown fox jumps over the lazy dog.';
+const str = 'The quick brown fox jumps over the lazy dog.';
 
 // const words = str.split(' '); // an array
 // console.log(words[3]);
-// // Expected output: "fox"
+// Expected output: "fox"
 
 // const chars = str.split('');
 // console.log(chars[8]);
