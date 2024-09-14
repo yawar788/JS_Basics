b8e21fa8f140e96989759e2158fdb8efbc0abe79
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-09-14 07:16:06 +0000

    some ef added in it

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,5 +1,34 @@
 // comparison
 
+// === — strict equality (triple equals)
+// == — loose equality (double equals)
+// Object.is()
+
+// console.log(NaN==NaN); // false
+// console.log(NaN!=NaN); // true
+// console.log(NaN===NaN); // false
+
+// console.log(+0==-0); // true
+// console.log(+0!=-0); // false
+// console.log(+0===-0); // true
+
+// const num = 0;
+// const obj = new String("0");
+// const str = "0";
+
+// console.log(num === num); // true
+// console.log(obj === obj); // true
+// console.log(str === str); // true
+
+// console.log(num === obj); // false
+// console.log(num === str); // false
+// console.log(obj === str); // false
+// console.log(null === undefined); // false
+// console.log(obj === null); // false
+// console.log(obj === undefined); // false
+
+
+
 
 // console.log(2>1);
 // console.log(2<1);
@@ -21,4 +50,4 @@
 
 // ==== 
 // checks same data types + value
-console.log("2"==2);
+// console.log("2"==2);
