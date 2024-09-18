402d06b43f0fc88f9be946acaefed07546bb873c
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-09-18 21:30:49 +0000

    edit

---
+++ 05_iteration/one.js
@@ -0,0 +1,29 @@
+// iterations 
+
+// for loop 
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element==5){ 
+        console.log("5 is the best number");
+        
+    };
+    console.log(element);
+      
+}
+
+
+for (let i = 0; i < 10; i++) {
+        console.log(`outer value is:${i}`);
+        
+    for (let j = 0; j < 10; j++) {
+    
+        console.log(`inner scope ${j} and inner loop is ${j}`);
+
+        
+        
+    }
+
+
+    function9
+
+}
