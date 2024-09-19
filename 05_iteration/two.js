da18f1dcd8aea13c3812f0ec864be12d03f2282b
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-09-19 18:05:52 +0000

    added while / do while definations

---
+++ 05_iteration/two.js
@@ -0,0 +1,26 @@
+// while / do while loops 
+
+
+// while simple
+// let i=0;
+// while(i<=10){
+//     console.log(`value of i is: ${i}`);
+//     i=i+2
+// }
+
+
+// while with array
+// let myArray=['flash','batman','superman']
+// let i = 0;
+// while(i<myArray.length){
+//     console.log(`value of i is: ${myArray[i]}`);
+//     i=i+1;
+// }
+
+
+// do while
+let i =11;
+do{
+   console.log(`value of i is ${i} `);
+    i=i+2;
+}while(i<=10)
