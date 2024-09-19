a38f7a686f8b83ec9dd5fadaaaacc44c4d93f42a
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-09-19 19:23:29 +0000

    advance

---
+++ 05_iteration/three.js
@@ -0,0 +1,17 @@
+// for of 
+
+// ['','','']  
+// [{},{},{}]
+
+// let arr=[1,2,3,4];
+// for(const num of arr){
+//     console.log(num);
+
+// }
+
+
+let greeting = "hello yawar"
+for (const greet of greeting) {
+    console.log(greet);
+    
+}
