982a1a3d97576ced7ec65c8d1f6e24f5ccdb02e3
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-09-19 07:12:32 +0000

    added some def  iterations + for loop + for loop in array + break / continue

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -4,26 +4,59 @@
 for (let i = 0; i <= 10; i++) {
     const element = i;
     if (element==5){ 
-        console.log("5 is the best number");
+        // console.log("5 is the best number");
         
     };
-    console.log(element);
+    // console.log(element);
       
 }
 
 
-for (let i = 0; i < 10; i++) {
-        console.log(`outer value is:${i}`);
+for (let i = 9; i <= 9; i++) {
+        // console.log(`outer value is:${i}`);
         
-    for (let j = 0; j < 10; j++) {
+     for (let j =1; j <= 10; j++) {
     
-        console.log(`inner scope ${j} and inner loop is ${j}`);
+        // console.log(`inner loop ${j} and inner loop is ${i}`);
+        // console.log(`${i} * ${j}  = ${i*j}`)
 
         
         
     }
 
+}
 
-    function9
 
+// array 
+const myArray=[2,3,4,5]
+
+for (let index = 3; index >=0; index--) {
+    const element = myArray[index];
+    // console.log(element);
+    
+    
+}
+
+
+// break and continue 
+
+// for (let i = 0; i <5; i++) {
+
+//     if (i===3) {
+//         console.log('detected 3');
+//         break;
+//     }
+//     console.log(`value of i is: ${i}`);
+    
+// }
+
+
+for (let i = 0; i <5; i++) {
+
+    if (i===3) {
+        console.log('detected 3');
+        continue;
+    }
+    console.log(`value of i is: ${i}`);
+    
 }
