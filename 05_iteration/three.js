e8c105f834f4fc79ce85cfc05b9d430e220af34d
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-09-20 17:31:12 +0000

    higher order loops + for of loop

--- 05_iteration/three.js
+++ 05_iteration/three.js
@@ -1,17 +1,50 @@
-// for of 
-
+// data base value 
 // ['','','']  
 // [{},{},{}]
 
-// let arr=[1,2,3,4];
-// for(const num of arr){
-//     console.log(num);
+// for of 
+let arr=[1,2,3,4];
+for(const num of arr){
+    // console.log(num);
 
-// }
+}
 
 
 let greeting = "hello yawar"
 for (const greet of greeting) {
-    console.log(greet);
+    // console.log(`Each char is : ${greet}`);
+    
+}
+
+// map
+// like objects 
+// take unique value 
+// follow order
+// iteratable
+
+
+const map=new Map();
+map.set('name','yawar');
+map.set('age','24');
+map.set('address','street-2 new pind pathan colony sukkr');
+// console.log(map);
+
+for (const [key,value] of map) {
+    // console.log(key,':-',value)
+}
+
+// object
+// not iteratable
+
+const myarr={
+    'gam1':'pubg',
+    'game2':'minimilitia',
+    'game2':'candycrash',
+    'game3':'subway'
+    
+}
+
+for (const [key,value] of myarr) {
+    console.log(key,':-',value)
     
 }
