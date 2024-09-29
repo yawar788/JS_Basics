2466c6ecd5a9cd229a28f75a6d9d7f106430b37f
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-09-29 10:14:09 +0000

    small class time table projects using objects

--- objects/object7.js
+++ objects/object7.js
@@ -1,14 +1,60 @@
 // BE - V 
 // making time table using objects 
 
-let schedule=[
-    {monday:{
-time
-    }},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-]
+let schedule ={
+    monday:{
+        '215-330':{
+            subject:'ecd'
+        },
+        '345-500':{
+            subject:'embeded'
+        }
+    },
+    tueday:{
+        '1015-1130':{
+            subject:'embeded'
+        },
+        '1200-115':{
+            subject:'ecd'
+        },
+        '215-330':{
+            subject:'instrument'
+        },
+        '345-500':{
+            subject:'probability'
+        },
+
+    },
+    wednesday:{
+        '1015-1130':{
+            subject:'signals'
+        },
+        '1200-115':{
+            subject:'instrument'
+        },
+        '215-330':{
+            subject:'ecd_lab'
+        },
+    },
+    thursday:{
+        '915-1015':{
+            subject:'embeded_lab'
+        },
+        '215-330':{
+            subject:'signals_lab'
+        }
+    },
+    friday:{
+        '915-1130':{
+            subject:'instrument_lab'
+        },
+        '215-330':{
+            subject:'signals'
+        },
+        '345-500':{
+            subject:'probability'
+        }
+    },
+}
+
+console.log(schedule);
