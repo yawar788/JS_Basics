474485ad5e476a56297bfa3820e9d074e4255af2
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-10-02 03:58:30 +0000

    date done

--- objects1/object6.js
+++ objects1/object6.js
@@ -1,7 +1,26 @@
+// Math Methods
 // Math.max(): Returns the largest of zero or more numbers.
-console.log(Math.max(1, 5, 10)); // Output: 10
-console.log(Math.min(1, 5, 10)); 
-console.log(Math.random()); 
-console.log(Math.floor(3.444)); 
-console.log(Math.ceil(3.444)); 
+// console.log(Math.max(1, 5, 10)); // Output: 10
+// console.log(Math.min(1, 5, 10)); 
+// console.log(Math.random()); 
+// console.log(Math.floor(3.444)); 
+// console.log(Math.ceil(3.444)); 
+
+// Date Methods
+// JavaScript provides the Date object to handle date and time operations.
+// Date.now(): Returns the number of milliseconds since January 1, 1970, 00:00:00 UTC.
+// console.log(Date.now());
+
+// Date.getFullYear(): Returns the year of the specified date according to local time.
+// let date = new Date();
+// console.log(date.getFullYear()); // Output: 2024
+
+// Date.getMonth(): Returns the month (0-11) in the specified date according to local time.
+// const month = new Date()
+// console.log(month.getMonth());
+
+
+// Date.toISOString(): Converts a date to a string following the ISO 8601 Extended Format.
+let date=new Date();
+console.log(date.toISOString());
 
