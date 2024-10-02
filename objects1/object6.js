3c157b00ec92d0a4cede7a9e477123705ae290ed
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-10-02 03:49:16 +0000

    math methods

---
+++ objects1/object6.js
@@ -0,0 +1,7 @@
+// Math.max(): Returns the largest of zero or more numbers.
+console.log(Math.max(1, 5, 10)); // Output: 10
+console.log(Math.min(1, 5, 10)); 
+console.log(Math.random()); 
+console.log(Math.floor(3.444)); 
+console.log(Math.ceil(3.444)); 
+
