e1854378b83ee492059be520c798f8e579ee81f7
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-10-05 21:10:57 +0000

    map, filter reduce done

--- 05_iteration/seven.js
+++ 05_iteration/seven.js
@@ -1,5 +1,10 @@
 let nums = [1,2,3,4,5];
-let newNums= nums.map((num)=>num+10)
+// let newNums= nums.map((num)=>num+10);
+
+let newNums=nums
+                .map((num)=>num * 10)
+                .map((num)=>num + 1)
+                .filter((num)=> num<50)
 console.log(newNums);
 
 
