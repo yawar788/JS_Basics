70fa762622c6557918472837f41206c7ddea1414
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-10-17 08:26:30 +0000

    index of + sor()

---
+++ arrays/array2.js
@@ -0,0 +1,9 @@
+// indexOf(element): Returns the first index at which a given element
+//  can be found, or -1 if it is not present.
+const fruits=['banana','cherry'];
+console.log(fruits.indexOf('kiwi'));
+
+// sort(): Sorts the elements of an array in place.
+fruits.sort(); // Sorts the array alphabetically
+
+
