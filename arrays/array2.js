677b354c549122d6a0df0e2e3f5937dafe45fbec
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-10-18 11:34:32 -0700

    Update array2.js
    added multidimensional array

--- arrays/array2.js
+++ arrays/array2.js
@@ -6,4 +6,12 @@ console.log(fruits.indexOf('kiwi'));
 // sort(): Sorts the elements of an array in place.
 fruits.sort(); // Sorts the array alphabetically
 
+fruits.reverse(); // Reverses the order of the array
 
+const martrix = [
+  [1,[1,2,3],2,3],
+  [4,5,6],
+  [7,8,9]
+];
+
+console.log(matrix[0][1][2]);
