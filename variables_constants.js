b0019caa95f3fdcb54fe37447dd1fee4e5a8b64f
Yawar Abbas <139193021+yawar788@users.noreply.github.com>
2024-11-18 20:46:38 -0800

    Update variables_constants.js
    add some basics of var variable

--- variables_constants.js
+++ variables_constants.js
@@ -1,1 +1,28 @@
+// Variables and Constants in JavaScript: var, let, and const
 
+// Introduction
+// In JavaScript, variables are used to store data that can be referenced
+// and manipulated later. There are three ways to declare variables: var,
+// let, and const. Each has unique behavior based on scope (global, block,
+// or local), hoisting, and immutability.
+
+// 1. var (Function Scope or Global Scope)
+// Characteristics:
+// 1. Function Scope: Variables declared with var are scoped to the function 
+//    in which they are defined. If declared outside a function, they are global.
+
+// 2. Re-declaration Allowed: You can re-declare a variable declared with var.
+// 3. Hoisting: Variables declared with var are hoisted to the top of their scope 
+//    but are initialized to undefined.
+
+// Example 1: Function Scope
+
+function testVar(){
+  if(true){
+    var x= 10;
+  }
+  console.log(x);
+}
+
+testVar();
+  
